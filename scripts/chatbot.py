f6ed24f9736a90342a7cd611dbf0a5d560bb15fb
silvhua <silvhua@gmail.com>
2023-09-24 08:32:01 -0700

    add chatbot interface

--- scripts/chatbot.py
+++ scripts/chatbot.py
@@ -39,16 +39,48 @@ conversation_dict[conversation_id] = create_chatbot(tool_dict[tool_id], streamli
 
 # Start the conversation
 """
-# Chatbot
+# RecyclePro Bot
 
 """
-query = st.text_input('Your question here') 
-answer_dict[conversation_id] = chat_with_chatbot(
-    query, conversation_dict[conversation_id], streamlit=True
-    ) 
+# Initialize chat history
+# Initialization
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'langchain_messages'
+    
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+print(f'\nsession state: {st.session_state["langchain_messages"]}')
+for index, message in enumerate(st.session_state['langchain_messages']):
+    if (message.content == ''):
+        pass
+    elif index % 2 == 0:
+        with st.chat_message("user"):
+            st.write(f'{message.content}')
+    else:
+        with st.chat_message("assistant"):
+            st.write(f'{message.content}')
 
-if st.button('Get results'):
+print(st.session_state.messages)
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo-16k"
+
+
+# prompt =  st.chat_input('Say something') 
+if prompt := st.chat_input("What is up?"):
+    answer_dict[conversation_id] = chat_with_chatbot(
+        prompt, conversation_dict[conversation_id], streamlit=True
+        ) 
     chatbot_response = answer_dict[conversation_id]['output'] 
-    st.write(chatbot_response)
-else:
-    st.write('Click for results')
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        st.markdown(chatbot_response)
+    
+
+# if st.button('Get results'):
+#     chatbot_response = answer_dict[conversation_id]['output'] 
+#     st.write(chatbot_response)
+# else:
+#     st.write('Click for results')
