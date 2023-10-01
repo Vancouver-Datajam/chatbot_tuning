9c06def037deefe8a97116f2195710d233920eb5
silvhua <silvhua@gmail.com>
2023-09-30 23:07:47 -0700

    modify so chat initiation timestamp is shown

--- scripts/chatbot.py
+++ scripts/chatbot.py
@@ -30,15 +30,16 @@ params_dict = {
 # if 'key' not in st.session_state:
 #     st.session_state['key'] = 'langchain_messages'
     
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-if 'streamlit' not in st.session_state:
-    st.session_state.streamlit = ''
-if 'embeddings_filepath' not in st.session_state:
-    st.session_state.embeddings_filepath = ''
-    st.write(f'Chat session initiated at {datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}')
     # Prepare the documents
     try: # on Streamlit
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        if 'streamlit' not in st.session_state:
+            st.session_state.streamlit = ''
+        if 'embeddings_filepath' not in st.session_state:
+            st.session_state.embeddings_filepath = ''
+            if 'chat_initiation' not in st.session_state:
+                st.session_state.chat_initiation = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
         doc_id = 1
         directory = 'data'
         doc_dict[doc_id] = create_documents(directory=directory, glob='*.csv')
@@ -78,6 +79,7 @@ conversation_dict[conversation_id] = create_chatbot(tool_dict[tool_id], streamli
 # RecyclePro Bot
 
 """
+st.write(f'Chat session initiated at {st.session_state.chat_initiation}')
 # Initialize chat history
 print(f'\nsession state messages: {st.session_state.messages}')
 for message in st.session_state.messages:
