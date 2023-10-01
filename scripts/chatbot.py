12953e3f44df0053366f098408db4f6ffcc42a4e
silvhua <silvhua@gmail.com>
2023-09-30 21:39:19 -0700

    save embeddings and reload them

--- scripts/chatbot.py
+++ scripts/chatbot.py
@@ -2,33 +2,56 @@ from chat_functions import *
 import streamlit as st
 from langchain.schema.messages import HumanMessage, AIMessage
 
-# Prepare the documents
-doc_id = 1
-try:
-    directory = 'data'
-    doc_dict[doc_id] = create_documents(directory=directory, glob='*.csv')
-except:
-    doc_dict[doc_id] = create_documents_from_csv()
-    print('Done creating doc from CSV')
-retriever_dict = create_retriever(doc_dict[doc_id], 'recycle')
-description_dict['recycle'] = """
+recylebc = """
 This document provides information from the Recycle BC website or BC government 
 website. It has the most specific information 
 about whether or not an item is accepted for recycling and where to recycle it.
 This should be the main resource for recycling information for residents of British Columbia.
 """
 
-doc_id = 2
-try:
-    directory = 'data'
-    doc_dict[doc_id] = create_documents(directory=directory, glob='*.txt', loader_cls=TextLoader)
-except:
-    directory = '../data'
-    doc_dict[doc_id] = create_documents(directory=directory, glob='*.txt', loader_cls=TextLoader)
-retriever_dict = create_retriever(doc_dict[doc_id], 'mattress')
-description_dict['mattress'] = """
+CoV_mattress = """
 Information from the City of Vancouver website about how to recycle mattresses.
 """
+
+params_dict = {
+    1: {
+        'site_key': 'recycle',
+        'doc_description': recylebc,
+        'text_splitter': None
+    },
+    2: {
+        'site_key': 'mattress',
+        'doc_description': CoV_mattress,
+        'text_splitter': None
+    }
+}
+
+# Prepare the documents
+try: # on Streamlit
+    doc_id = 1
+    directory = 'data'
+    doc_dict[doc_id] = create_documents(directory=directory, glob='*.csv')
+
+    doc_id = 2
+    doc_dict[doc_id] = create_documents(directory=directory, glob='*.txt', loader_cls=TextLoader)
+    embeddings_filepath = 'embeddings/'
+    streamlit = True
+    print('Executing on Streamlit')
+except: # on local machine
+    doc_id = 1
+    doc_dict[doc_id] = create_documents_from_csv()
+    print('Done creating doc from CSV')
+
+    doc_id = 2
+    directory = '../data'
+    doc_dict[doc_id] = create_documents(directory=directory, glob='*.txt', loader_cls=TextLoader)
+    embeddings_filepath = '../embeddings/'
+    streamlit = False
+    print('Executing on local machine')
+
+retriever_dict, description_dict = create_retriever_and_description_dicts(params_dict, embeddings_filepath)
+
+# Create tools
 tool_id = 1
 tool_dict[tool_id] = create_tools_list(retriever_dict, description_dict)
 
@@ -36,7 +59,7 @@ tool_dict[tool_id] = create_tools_list(retriever_dict, description_dict)
 conversation_id = 1
 input_id = 1
 
-conversation_dict[conversation_id] = create_chatbot(tool_dict[tool_id], streamlit=True)
+conversation_dict[conversation_id] = create_chatbot(tool_dict[tool_id], streamlit=streamlit)
 
 # Start the conversation
 """
@@ -71,7 +94,7 @@ if "openai_model" not in st.session_state:
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     answer_dict[conversation_id] = chat_with_chatbot(
-        prompt, conversation_dict[conversation_id], streamlit=True
+        prompt, conversation_dict[conversation_id], streamlit=streamlit
         ) 
     chatbot_response = answer_dict[conversation_id]['output'] 
     st.session_state.messages.append({"role": "assistant", "content": chatbot_response})
